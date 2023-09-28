d696b41d5133cd1af86d5de6849c992a66f1e437
eaten <76576856+eatenliao@users.noreply.github.com>
2023-09-28 21:31:26 +0800

    [SERVER] changed cors to server,js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 var app = require('./controller/app');
 var PORT = process.env.PORT || 8081;
 var server = process.env.SERVER || 'localhost';
+var cors = require('cors');
+
+// Allow requests from specific origins
+const allowedOrigins = ['https://gds-swe-gcc2-client.vercel.app'];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 app.listen(PORT, server, () => {
     console.log(`Server is listening on ${server}:${PORT}`);
